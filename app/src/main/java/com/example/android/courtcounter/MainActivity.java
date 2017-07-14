cc681f9cdd3706906b4a974269457454e37e34bd
TomislavNovacic <tomislav.novacic2@gmail.com>
2017-07-14 14:42:35 +0200

    Cleaned old code.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.courtcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         displayForTeamA(0);
         displayForTeamB(0);
-    }
+
+        ArrayList<Button> buttonsList = new ArrayList<>();
+
+        Button increment3A = (Button) findViewById(R.id.increment3a);
+        buttonsList.add(increment3A);
+        Button increment2A = (Button) findViewById(R.id.increment2a);
+        buttonsList.add(increment2A);
+        Button increment1A = (Button) findViewById(R.id.increment1a);
+        buttonsList.add(increment1A);
+        Button increment3B = (Button) findViewById(R.id.increment3b);
+        buttonsList.add(increment3B);
+        Button increment2B = (Button) findViewById(R.id.increment2b);
+        buttonsList.add(increment2B);
+        Button increment1B = (Button) findViewById(R.id.increment1b);
+        buttonsList.add(increment1B);
+        Button reset = (Button) findViewById(R.id.resetButton);
+        buttonsList.add(reset);
+
+        for (int i = 0; i < buttonsList.size(); i++) {
+            final int finalI = i;
+            buttonsList.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(finalI == 0) {
+                        scoreTeamA = scoreTeamA + 3;
+                        displayForTeamA(scoreTeamA);
+                    }
+                    if(finalI == 1) {
+                        scoreTeamA = scoreTeamA + 2;
+                        displayForTeamA(scoreTeamA);
+                    }
+                    if(finalI == 2) {
+                        scoreTeamA = scoreTeamA + 1;
+                        displayForTeamA(scoreTeamA);
+                    }
+                    if(finalI == 3) {
+                        scoreTeamB = scoreTeamB + 3;
+                        displayForTeamB(scoreTeamB);
+                    }
+                    if(finalI == 4) {
+                        scoreTeamB = scoreTeamB + 2;
+                        displayForTeamB(scoreTeamB);
+                    }
+                    if(finalI == 5) {
+                        scoreTeamB = scoreTeamB + 1;
+                        displayForTeamB(scoreTeamB);
+                    }
+                    if(finalI == 6) {
+                        scoreTeamA = 0;
+                        scoreTeamB = 0;
+                        displayForTeamA(scoreTeamA);
+                        displayForTeamB(scoreTeamB);
+                    }
+                    }
+                });
+            }
+        }
+
     /**
      * Displays the given score for Team A.
      */
@@ -30,36 +90,5 @@ public class MainActivity extends AppCompatActivity {
     public void displayForTeamB(int scoreTeamB) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(scoreTeamB));
-    }
-    public void increment3 (View view) {
-       scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
-    }
-    public void increment2 (View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
-    }
-    public void increment1 (View view) {
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
-    }
-    public void increment3b (View view) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
-    }
-    public void increment2b (View view) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
-    }
-    public void increment1b (View view) {
-        scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
-    }
-
-    public void resetButtom (View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
     }
 }
